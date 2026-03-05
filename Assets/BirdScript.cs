5cfa02c8f93f14f5c40e5c69a9ae278b0a4d2f0b
kevinkrantz-LBS <kevin.krantz@elev.ga.lbs.se>
2026-03-05 12:15:34 +0100

    bird up and down

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -3,23 +3,25 @@ using UnityEngine;
 public class BirdScript : MonoBehaviour
 {
     public Rigidbody2D myRigidBody; // referens till birdens Rigidbody
-    public float flapStrength; // hur starkt hoppet ska vara
-    public bool birdIsAlive = true; // används senare för game over
+    public float flapStrength; // hur starkt hoppet är
+    public bool birdIsAlive = true; // används senare när man dör
 
-    // Start körs när spelet startar
     void Start()
     {
 
     }
 
-    // Update körs varje frame
     void Update()
     {
-        // kollar om spelaren trycker space och om fågeln fortfarande lever
         if (Input.GetKeyDown(KeyCode.Space) && birdIsAlive)
         {
-            // ger fågeln en uppåt kraft
+            // gör så fågeln/fisken hoppar upp
             myRigidBody.linearVelocity = Vector2.up * flapStrength;
         }
+
+        // roterar fisken beroende på om den åker upp eller ner
+        float rotation = myRigidBody.linearVelocity.y * 5;
+        rotation = Mathf.Clamp(rotation, -90, 45);
+        transform.rotation = Quaternion.Euler(0, 0, rotation);
     }
 }
