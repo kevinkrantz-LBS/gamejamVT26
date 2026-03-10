e55723e31a199c95581619ff461264c83b340205
kevinkrantz-LBS <kevin.krantz@elev.ga.lbs.se>
2026-03-10 09:13:30 +0100

    points fix

--- Assets/LogicScript.cs
+++ Assets/LogicScript.cs
@@ -2,39 +2,44 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+public class LogicScript : MonoBehaviour
+{
+    // håller koll på spelarens poäng
+    public int playerScore;
 
-    public class LogicScript : MonoBehaviour
+    // referens till texten som visar score på skärmen
+    public Text scoreText;
+
+    // game over UI som visas när man dör
+    public GameObject gameOverScreen;
+
+    // denna funktion körs när spelaren får poäng
+    public void addScore(int scoreToAdd)
     {
-        // håller koll på spelarens poäng
-        public int playerScore;
+        // lägger till poäng
+        playerScore = playerScore + scoreToAdd;
 
-        // referens till texten som visar score på skärmen
-        public Text scoreText;
-
-        // game over UI som visas när man dör
-        public GameObject gameOverScreen;
-
-        // denna funktion körs när spelaren får poäng
-        public void addScore(int scoreToAdd)
+        // uppdaterar texten så rätt score visas
+        if (scoreText != null)
         {
-            // lägger till poäng
-            playerScore = playerScore + scoreToAdd;
-
-            // uppdaterar texten så rätt score visas
             scoreText.text = playerScore.ToString();
         }
+    }
 
-        // körs när spelaren dör
-        public void gameOver()
+    // körs när spelaren dör
+    public void gameOver()
+    {
+        // gör så game over screen syns
+        if (gameOverScreen != null)
         {
-            // gör så game over screen syns
             gameOverScreen.SetActive(true);
         }
-
-        // startar om spelet
-        public void restartGame()
-        {
-            // laddar om samma scen igen
-            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-        }
     }
+
+    // startar om spelet
+    public void restartGame()
+    {
+        // laddar om samma scen igen
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
